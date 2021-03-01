a672df20e874993b8169fc550c1d713f77bbc76d
Renato Andrade <renatorobot@outlook.com.br>
2021-03-01 09:39:03 -0300

    Resposta desafio 1

---
+++ desafio-01/app.js
@@ -0,0 +1,12 @@
+new Vue({
+
+    el: '#desafio',
+    data: {
+
+        nome: 'Mark Zuckerberg',
+        idade: 36,
+        imagem: 'https://media.wired.com/photos/592676467034dc5f91beb80e/master/pass/MarkZuckerberg.jpg'
+
+    }
+
+})
